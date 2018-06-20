28de531a85c97275ea5064aa55584e1eab30f40f
maritimememory <lkbrink0930@gmail.com>
2018-06-20 19:21:59 +0000

    automatically backed up by learn

--- array_and_hashes.rb
+++ array_and_hashes.rb
@@ -4,3 +4,26 @@ ingredients = [
   ["swiss", "munster", "chedder"],
   ["mayo", "mustard", "tabasco"]
   ]
+
+cities = {
+  :new_york => { 
+    :mayor => "Bill DeBlasio",
+    :population => 8337000,
+    :website => "http://www.nyc.gov"
+  },
+  :los_angeles => { 
+    :mayor => "Eric Garcetti",
+    :population => 3884307,
+    :website => "http://www.lacity.org"
+  },
+  :miami => { 
+    :mayor => "Tomás Regalado",
+    :population => 419777,
+    :website => "http://www.miamigov.com"
+  },
+  :chicago => { 
+    :mayor => "Rahm Emanuel",
+    :population => 2695598,
+    :website => "http://www.cityofchicago.org/"
+  }
+}
